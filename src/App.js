f51227323019e2515fc15211672d738568ee6a69
ATesner <zertaps@gmail.com>
2017-11-12 16:36:20 +0100

    appjs !

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
     }
 
     render() {
+        console.log('Appjs !')
         //if the user and all chats is set (if I submit a login) show the chat
         if(this.props.user && this.props.chats){
             return (
